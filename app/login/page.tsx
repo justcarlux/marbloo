0a349f44622cbdcf0da79382e46fa1a917692cbc
justcarlux <manuelitojuan691@gmail.com>
2026-04-29 22:41:13 -0400

    fix(login-signup): :bug: redirect to `/` if the user is already logged in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,21 @@
 import { Metadata } from "next";
 import LogInForm from "../components/ui/LogInForm";
+import { createSupabaseServerClient } from "@/lib/supabase/server-client";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
     title: "Log In | Marbloo",
 };
 
-export default function LoginPage() {
+export default async function LoginPage() {
+    const supabase = await createSupabaseServerClient();
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    if (user) {
+        redirect("/");
+    }
+
     return <LogInForm />;
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,21 @@
 import { Metadata } from "next";
 import SignUpForm from "../components/ui/SignUpForm";
+import { createSupabaseServerClient } from "@/lib/supabase/server-client";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
     title: "Sign Up | Marbloo",
 };
 
-export default function LoginPage() {
+export default async function SignUpPage() {
+    const supabase = await createSupabaseServerClient();
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    if (user) {
+        redirect("/");
+    }
+
     return <SignUpForm />;
 }
